b05470c39af1d7c9dc9e554ec8176d4a40212226
Aydar <aydar710@gmail.com>
2021-01-07 23:43:11 +0300

    add receiving some user info from vk api

---
+++ vkapi/VK_API.py
@@ -0,0 +1,73 @@
+import vk_api
+import requests
+
+api_version = "5.126"
+token = 'f047c883f6419bdf4fa39be425dc7a2c9c3ff6e07226d5386467f11d83f3fe961cd1c40f99f31f844aeb5'
+BASE_VK_URL = 'https://api.vk.com/method/'
+
+
+class VK_API(object):
+
+    def __init__(self, screen_name):
+        self.screen_name = screen_name
+
+    # returns access_token
+    def authorize(self):
+        vk_session = vk_api.VkApi('aydar710@gmail.com', 'ayd148868')
+        vk_session.auth()
+        return vk_session.token['access_token']
+
+    def get_sex_and_followers_count_and_userId(self):
+        profile_info_url = BASE_VK_URL + "users.get"
+        user_info_response = requests.get(profile_info_url, params={
+            'access_token': token,
+            'v': api_version,
+            'user_ids': self.screen_name,
+            'fields': 'sex, followers_count, personal'
+        }).json()['response'][0]
+
+        # print(user_info_response)
+        sex = user_info_response['sex']
+        followers_count = user_info_response['followers_count']
+        user_id = user_info_response['id']
+        return sex, followers_count, user_id
+
+    #:param user_id: Int
+    #:return friends_count : Int
+    def get_friends_count(self, user_id):
+        friends_url = BASE_VK_URL + "friends.get"
+
+        friends_response = requests.get(friends_url, params={
+            'access_token': token,
+            'v': api_version,
+            'user_id': user_id,
+        }).json()['response']
+
+        return friends_response['count']
+
+    #:param user_id : Int
+    #:return video_count : Int
+    def get_video_count(self, user_id):
+        video_url = BASE_VK_URL + 'video.get'
+
+        video_response = requests.get(video_url, params={
+            'access_token': token,
+            'v': api_version,
+            'owner_id': user_id
+        }).json()['response']
+
+        return video_response['count']
+
+    #:param user_id : Int
+    #:return profile_photos_count : Int
+    def get_profile_photos_count(self, user_id):
+        photos_url = BASE_VK_URL + 'photos.get'
+
+        photos_response = requests.get(photos_url, params={
+            'access_token': token,
+            'v': api_version,
+            'owner_id': user_id,
+            'album_id': 'profile'
+        }).json()['response']
+
+        return photos_response['count']
---
+++ vkapi/testVkApi.py
@@ -0,0 +1,16 @@
+from VK_API import VK_API
+
+vk_api = VK_API('nursabir')
+sex, followers_count, user_id = vk_api.get_sex_and_followers_count_and_userId()
+# print(sex)
+# print(followers_count)
+# print(user_id)
+
+# friends_count = vk_api.get_friends_count(user_id)
+# print(friends_count)
+
+# video_count = vk_api.get_video_count(user_id)
+# print(video_count)
+
+profile_photos_count = vk_api.get_profile_photos_count(user_id)
+print(profile_photos_count)
