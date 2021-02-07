435e475925b7c9d836d60ba2a20b01707c76877b
Aydar <aydar710@gmail.com>
2021-02-07 23:25:11 +0300

    fix forming classes

--- strategy/StrategyTest.py
+++ strategy/StrategyTest.py
@@ -4,7 +4,7 @@ from strategy.RandomForestStrategy import RandomForestStrategy
 
 random_forest = RandomForestStrategy()
 mlp_strategy = MLPStrategy()
-oracle = Oracle(mlp_strategy)
+oracle = Oracle(random_forest)
 
 predictions = oracle.predict_classes([1, 401, 220, 43, 3])
 print(predictions)
--- trainingsample/Form_Classes.py
+++ trainingsample/Form_Classes.py
@@ -11,20 +11,20 @@ answers_frame = pandas.read_csv('../answerprocessing/answers.csv', delimiter=','
 # X = numpy.zeros((5, 18))
 
 # making classes
-for i in range(len(answers_frame['O  '])):
-    answers_frame['O  '][i] = get_class_from_score(answers_frame['O  '][i])
+for i in range(len(answers_frame['O'])):
+    answers_frame['O'][i] = get_class_from_score(answers_frame['O'][i])
 
-for i in range(len(answers_frame['C  '])):
-    answers_frame['C  '][i] = get_class_from_score(answers_frame['C  '][i])
+for i in range(len(answers_frame['C'])):
+    answers_frame['C'][i] = get_class_from_score(answers_frame['C'][i])
 
-for i in range(len(answers_frame['E  '])):
-    answers_frame['E  '][i] = get_class_from_score(answers_frame['E  '][i])
+for i in range(len(answers_frame['E'])):
+    answers_frame['E'][i] = get_class_from_score(answers_frame['E'][i])
 
-for i in range(len(answers_frame['A  '])):
-    answers_frame['A  '][i] = get_class_from_score(answers_frame['A  '][i])
+for i in range(len(answers_frame['A'])):
+    answers_frame['A'][i] = get_class_from_score(answers_frame['A'][i])
 
-for i in range(len(answers_frame['N  '])):
-    answers_frame['N  '][i] = get_class_from_score(answers_frame['N  '][i])
+for i in range(len(answers_frame['N'])):
+    answers_frame['N'][i] = get_class_from_score(answers_frame['N'][i])
 
 answers_frame.to_csv('../answerprocessing/answers.csv', index=False)
 
