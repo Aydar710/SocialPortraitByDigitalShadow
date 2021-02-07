976319180bf80b125464ea2e8718cdd376c5dff2
Aydar <aydar710@gmail.com>
2021-02-07 23:09:20 +0300

    fix forming classes from score

--- featuresfromvk/Formation_classes.py
+++ featuresfromvk/Formation_classes.py
@@ -6,7 +6,7 @@ from answerprocessing.TestResultParser import ANSWERS_CSV
 def get_class_from_score(score: int) -> int:
     if 1 <= score <= 2.9: return 1
     if 3 <= score <= 5: return 2
-    if 5.1 <= score <= 7: return 1
+    if 5.1 <= score <= 7: return 3
 
 
 answers_frame = pandas.read_csv('../answerprocessing/answers.csv', delimiter=',')
